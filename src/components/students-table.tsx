d55362b664450fd343a0db0cee35b116cfa0e682
coronado03 <sebascoronado03@hotmail.com>
2026-03-26 22:28:50 +0100

    initial vistula.network layout

---
+++ src/components/students-table.tsx
@@ -0,0 +1,40 @@
+
+export default function StudentsTable() {
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>program</th>
+          <th>site</th>
+          <th>links</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Chris</th>
+          <td>HTML tables</td>
+          <td>22</td>
+        </tr>
+        <tr>
+          <th>Dennis</th>
+          <td>Web accessibility</td>
+          <td>45</td>
+        </tr>
+        <tr>
+          <th>Sarah</th>
+          <td>JavaScript frameworks</td>
+          <td>29</td>
+        </tr>
+        <tr>
+          <th>Karen</th>
+          <td>Web performance</td>
+          <td>36</td>
+        </tr>
+      </tbody>
+
+
+    </table>
+  )
+}
